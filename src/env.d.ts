2f1547e319c97a654943eb0e12e3d64533b299a3
Vinskao <tianyikao@gmail.com>
2025-07-09 20:26:24 +0800

    info: import json

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// 允許 TypeScript 直接 import JSON 檔案
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
